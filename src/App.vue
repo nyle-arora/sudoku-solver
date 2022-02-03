6a93f587afa9393a0ac833f482b83ebe62979dd8
Nyle Arora <nylearora2021@u.northwestern.edu>
2022-02-02 21:59:52 -0600

    no soln properly captured

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@
 
 <script>
 import Puzzle from './components/Puzzle.vue';
-import { solvePuzzle, /*preSolveLegalCheck*/ } from './Solver';
+import { solvePuzzle, preSolveLegalCheck } from './Solver';
 
 export default {
   name: 'App',
@@ -84,26 +84,26 @@ export default {
     solveAlgoActivated() {
       this.loader = true;
       console.log('ready to solve');
-      let soln = solvePuzzle(this.gridArray);
-      console.log('made it');
-      if (soln.solvable) {
-        this.solvedGridArray = soln.arr;
-      } else {
-        console.log('in here');
-        this.noSoln();
-      }
-      // if (preSolveLegalCheck(this.gridArray) == false){
-      //   this.noSoln();
+      // let soln = solvePuzzle(this.gridArray);
+      // console.log('made it');
+      // if (soln.solvable) {
+      //   this.solvedGridArray = soln.arr;
       // } else {
-      //   let soln = solvePuzzle(this.gridArray);
-      //   console.log('made it');
-      //   if (soln.solvable) {
-      //     this.solvedGridArray = soln.arr;
-      //   } else {
-      //     console.log('in here');
-      //     this.noSoln();
-      //   }
+      //   console.log('in here');
+      //   this.noSoln();
       // }
+      if (preSolveLegalCheck(this.gridArray) == false){
+        this.noSoln();
+      } else {
+        let soln = solvePuzzle(this.gridArray);
+        console.log('made it');
+        if (soln.solvable) {
+          this.solvedGridArray = soln.arr;
+        } else {
+          console.log('in here');
+          this.noSoln();
+        }
+      }
       this.loader = false;
     },
     noSoln() {
--- src/Solver.js
+++ src/Solver.js
@@ -1,8 +1,41 @@
 function preSolveLegalCheck(gridArray) {
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            let isLegal = rowLegal(gridArray[i][j], i, gridArray) && colLegal(gridArray[i][j], j, gridArray) && boxLegal(gridArray[i][j], i, j, gridArray);
+            console.log(gridArray[i][j]);
+            let isLegal = preSolveRowCheck(gridArray[i][j], i, j, gridArray) && preSolveColCheck(gridArray[i][j], i, j, gridArray) && preSolveBoxCheck(gridArray[i][j], i, j, gridArray);
             if (isLegal == false) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function preSolveRowCheck(num, rowIndex, colIndex, gridArray) {
+    for(let i = 0; i < 9; i++) {
+        if (gridArray[rowIndex][i] == num && num != 0 && i != colIndex) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function preSolveColCheck(num, rowIndex, colIndex, gridArray) {
+    for (let i = 0; i < 9; i++) {
+        if (gridArray[i][colIndex] == num && num != 0 && i != rowIndex) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function preSolveBoxCheck(num, rowIndex, colIndex, gridArray) {
+    let leftCornerRow = rowIndex - rowIndex % 3;
+    let leftCornerCol = colIndex - colIndex % 3;
+
+    for (let i = leftCornerRow; i < leftCornerRow + 3; i++) {
+        for (let j = leftCornerCol; j < leftCornerCol + 3; j++) {
+            if (gridArray[i][j] == num && num != 0 && i != rowIndex && j != colIndex) {
                 return false;
             }
         }
@@ -54,8 +87,9 @@ function solvePuzzle(gridArray) {
 }
 
 function rowLegal(num, rowIndex, gridArray) {
+    console.log('num is', num);
     for(let i = 0; i < 9; i++) {
-        if (gridArray[rowIndex][i] == num) {
+        if (gridArray[rowIndex][i] == num && num != 0) {
             return false;
         }
     }
@@ -64,7 +98,7 @@ function rowLegal(num, rowIndex, gridArray) {
 
 function colLegal(num, colIndex, gridArray) {
     for (let i = 0; i < 9; i++) {
-        if (gridArray[i][colIndex] == num) {
+        if (gridArray[i][colIndex] == num && num != 0) {
             return false;
         }
     }
@@ -78,7 +112,7 @@ function boxLegal(num, rowIndex, colIndex, gridArray) {
 
     for (let i = leftCornerRow; i < leftCornerRow + 3; i++) {
         for (let j = leftCornerCol; j < leftCornerCol + 3; j++) {
-            if (gridArray[i][j] == num) {
+            if (gridArray[i][j] == num && num != 0) {
                 return false;
             }
         }
